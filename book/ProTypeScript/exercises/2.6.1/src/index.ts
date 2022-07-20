3ceb32751dacbea894118c9a9769cb7e577a3ba4
Shingo Hiraya <shingohiraya@gmail.com>
2022-07-20 17:51:31 +0900

    Update index.ts

--- book/ProTypeScript/exercises/2.6.1/src/index.ts
+++ book/ProTypeScript/exercises/2.6.1/src/index.ts
@@ -1,2 +1,30 @@
-const message: string = "Hello, world!";
-console.log(message);
+const start = 1;
+const end = 100;
+
+let result = "";
+
+for (let index = start; index <= end; index++) {
+    const isMultipleOf3 = ((index % 3) === 0)
+    const isMultipleOf5 = ((index % 5) === 0)
+
+    if (isMultipleOf3 && isMultipleOf5) {
+        // 3 の倍数であり、5 の倍数でもある
+        // console.log("FizzBuzz");
+        result += "FizzBuzz"
+    } else if (isMultipleOf3) {
+        // 3 の倍数であり、5 の倍数でない
+        // console.log("Fizz");
+        result += "Fizz"
+    } else if (isMultipleOf5) {
+        // 5 の倍数であり、3 の倍数でない
+        // console.log("Buzz");
+        result += "Buzz"
+    } else {
+        // 3 の倍数でなく、5 の倍数でもない
+        // console.log(index);
+        result += String(index)
+    }
+    result += " "
+}
+
+console.log(result);
